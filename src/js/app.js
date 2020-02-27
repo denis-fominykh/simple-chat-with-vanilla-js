1593aa603230c6bb15613a2308c25c1a72fd5b18
denis-fominykh <fominykhdenis@gmail.com>
2020-02-27 23:36:53 +0200

    displaying the user's iframes

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
 import { UI } from './ui';
+import { IFrame } from './iframe';
 
-UI.displayIFrame();
+document.addEventListener('DOMContentLoaded', UI.displayIFrame);
+
+document
+  .querySelector('.iframe-creator__user-creator')
+  .addEventListener('click', () => {
+    const name = document.querySelector('.iframe-creator__name-input').value;
+    if (name !== '') {
+      UI.addIFrameToChat(new IFrame(name));
+    }
+  });
