7069868a66ff517518c47aab637e6b5d80d509e7
denis-fominykh <fominykhdenis@gmail.com>
2020-03-03 23:08:16 +0200

    creating the events listeners

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,38 @@
-import { UI } from './ui';
 import { IFrame } from './iframe';
 import { Mediator } from './mediator';
 
-const mediator = new Mediator();
+document.addEventListener('DOMContentLoaded', () => {
+  const mediator = new Mediator();
 
-document.addEventListener('DOMContentLoaded', UI.displayIFrame(mediator));
+  document
+    .querySelector('.iframe-creator__user-creator')
+    .addEventListener('click', () => {
+      const name = document.querySelector('.iframe-creator__name-input');
+      if (name.value !== '') {
+        mediator.addIFrame(new IFrame(name.value));
+        name.value = '';
+      }
+    });
 
-document
-  .querySelector('.iframe-creator__user-creator')
-  .addEventListener('click', () => {
-    const name = document.querySelector('.iframe-creator__name-input').value;
-    if (name !== '') {
-      mediator.addIFrame(new IFrame(name));
-      UI.addIFrameToChat(new IFrame(name));
-    }
-  });
+  document
+    .querySelector('.iframe-chat')
+    .addEventListener('click', event => {
+      if (event.target.classList.contains('iframe__delete-btn')) {
+        mediator.deleteIFrame(event.target);
+      }
 
-document.querySelector('.iframe-chat').addEventListener('click', event => {
-  UI.deleteIFrame(event.target);
+      const message = event.target.previousSibling.previousSibling;
+      const from = event.target
+        .previousSibling
+        .previousSibling
+        .previousSibling
+        .previousSibling;
+      if (
+        message.classList.contains('iframe__own-message') ||
+        from.classList.contains('iframe__name')
+      ) {
+        mediator.send(message.value, from.textContent);
+        message.value = '';
+      }
+    });
 });
